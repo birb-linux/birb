8486744f6601f91f3a1709b07ebc510c6a871044
Toasterbirb <git@toasterbirb.com>
2023-04-17 17:51:10 +0300

    Create a project base

---
+++ Makefile
@@ -0,0 +1,12 @@
+CXX=g++
+CXX_FLAGS=-O2
+
+SRC_DIR=./src
+BUILD_DIR=./build
+
+all:
+	mkdir -p ${BUILD_DIR}
+	${CXX} ${CXX_FLAGS} ${SRC_DIR}/dep_solver.cpp -o ${BUILD_DIR}/birb_dep_solver
+
+clean:
+	rm -rf ${BUILD_DIR}
---
+++ src/dep_solver.cpp
@@ -0,0 +1,11 @@
+/**************************************************/
+/* Dependency solver for the birb package manager */
+/**************************************************/
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Finish this program :P" << std::endl;
+	return 0;
+}
