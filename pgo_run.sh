669790328c6cdd7af6a9048801a184b9d2e17915
Toasterbirb <git@toasterbirb.com>
2023-09-13 10:30:07 +0300

    Fix the rest of the shellcheck errors

--- pgo_run.sh
+++ pgo_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_DIR="$(dirname $0)"
+BUILD_DIR="$(dirname "$0")"
 BINARIES_TO_RUN="birb_db birb_dep_solver birb_pkg_search"
 
 # Make sure that the build directory exists
@@ -25,25 +25,25 @@ done
 _birb_db()
 {
 	echo "Running: birb_db"
-	${BUILD_DIR}/birb_db --update this_package_will_not_exist 1.2.3
-	${BUILD_DIR}/birb_db --list &>/dev/null
-	${BUILD_DIR}/birb_db --remove this_package_will_not_exist
-	${BUILD_DIR}/birb_db --is-installed ncurses &>/dev/null
-	${BUILD_DIR}/birb_db --is-installed alweiuhlawiuefhu &>/dev/null
+	"${BUILD_DIR}/birb_db" --update this_package_will_not_exist 1.2.3
+	"${BUILD_DIR}/birb_db" --list &>/dev/null
+	"${BUILD_DIR}/birb_db" --remove this_package_will_not_exist
+	"${BUILD_DIR}/birb_db" --is-installed ncurses &>/dev/null
+	"${BUILD_DIR}/birb_db" --is-installed alweiuhlawiuefhu &>/dev/null
 }
 
 _birb_dep_solver()
 {
 	echo "Running: birb_dep_solver"
-	${BUILD_DIR}/birb_dep_solver dde &>/dev/null
-	${BUILD_DIR}/birb_dep_solver firefox &>/dev/null
-	${BUILD_DIR}/birb_dep_solver -r ncurses &>/dev/null
+	"${BUILD_DIR}/birb_dep_solver" dde &>/dev/null
+	"${BUILD_DIR}/birb_dep_solver" firefox &>/dev/null
+	"${BUILD_DIR}/birb_dep_solver" -r ncurses &>/dev/null
 }
 
 _birb_pkg_search()
 {
 	echo "Running: birb_pkg_search"
-	${BUILD_DIR}/birb_pkg_search iceauth luit mkfontscale sessreg setxkbmap smproxy x11perf xauth xbacklight xcmsdb xcursorgen xdpyinfo xdriinfo xev xgamma xhost xinput xkbcomp xkbevd xkbutils xkill xlsatoms xlsclients xmessage xmodmap xpr xprop xrandr xrdb xrefresh xset xsetroot xvinfo xwd xwininfo xwud &>/dev/null
+	"${BUILD_DIR}/birb_pkg_search" iceauth luit mkfontscale sessreg setxkbmap smproxy x11perf xauth xbacklight xcmsdb xcursorgen xdpyinfo xdriinfo xev xgamma xhost xinput xkbcomp xkbevd xkbutils xkill xlsatoms xlsclients xmessage xmodmap xpr xprop xrandr xrdb xrefresh xset xsetroot xvinfo xwd xwininfo xwud &>/dev/null
 }
 
 # Run the functions
