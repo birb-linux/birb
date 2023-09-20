c0b58cf0c1bbfa4da4924539447671b6130084de
Toasterbirb <git@toasterbirb.com>
2023-09-20 22:21:34 +0300

    Make the appimage scraper work with firefox

--- scripts/appimagehub.sh
+++ scripts/appimagehub.sh
@@ -3,5 +3,4 @@ cacheFile="$HOME/.cache/appimagehub_tmp"
 [ -e $cacheFile ] || wget -O $cacheFile "https://appimage.github.io/apps/"
 program=$(grep "<tr id=" $cacheFile | sed 's/^[[:space:]]*//g; s/<tr id="\///g; s/\/">//g' | fzf)
 
-[ -e ${cacheFile}_${program} ] || wget -O ${cacheFile}_${program} "https://appimage.github.io/$program/"
-surf ${cacheFile}_${program}
+firefox "https://appimage.github.io/$program/"
